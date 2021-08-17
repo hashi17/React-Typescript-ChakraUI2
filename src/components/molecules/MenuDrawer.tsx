27df3ba65829778dba64379206848fbe4ef2bda5
hashi17 <h.mabo3@gmail.com>
2021-08-17 16:49:48 +0900

    ルーティングの実装

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -8,20 +8,35 @@ import {
 } from "@chakra-ui/react";
 
 type Props = {
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
   onClose: () => void;
   isOpen: boolean;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const {
+    onClickHome,
+    onClickUserManagement,
+    onClickSetting,
+    onClose,
+    isOpen
+  } = props;
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button width="100%">TOP</Button>
-            <Button width="100%">ユーザ一覧</Button>
-            <Button width="100%">設定</Button>
+            <Button width="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button width="100%" onClick={onClickUserManagement}>
+              ユーザ一覧
+            </Button>
+            <Button width="100%" onClick={onClickSetting}>
+              設定
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
