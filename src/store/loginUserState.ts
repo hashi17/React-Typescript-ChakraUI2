4dfc0135dc443f6e5a82f27dc6c3030e2a990ee3
hashi17 <h.mabo3@gmail.com>
2021-08-21 16:41:30 +0900

    フラグによって詳細画面を編集できるようにする

--- src/store/loginUserState.ts
+++ src/store/loginUserState.ts
@@ -3,5 +3,5 @@ import { User } from "../types/api/user";
 
 export const loginUserState = atom({
   key: "loginUserState",
-  default: <User | null>{}
+  default: <(User & { isAdmin: boolean }) | null>{}
 });
